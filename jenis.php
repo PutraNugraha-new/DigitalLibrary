83b844d27253da0262e1f821cefbbcb09cb9656c
PutraNugraha-new <putranugraha803@gmail.com>
2022-05-18 08:43:18 +0700

    merapikan sedikit

--- jenis.php
+++ jenis.php
@@ -39,12 +39,14 @@ include_once('header.php');
 							<label class="modal-bg" for="modal-<?php echo $dapat->id; ?>"></label>
 							<div class="modal-body">
 								<h4 class="modal-title">Hapus Data Koleksi</h4>
+
 								<?php if ($jenis->jenisKoleksi('view_koleksi', $dapat->id) == 0) { ?>
 									<p class="modal-text">Anda yakin akan menghapus data ini (ID=<?php echo $dapat->id; ?>)?</p>
 									<a href="jenis_hapus.php?id=<?php echo $dapat->id; ?>" class="paper-btn btn-small btn-danger">Ya, hapus!</a>
 								<?php } else { ?>
 									<p class="modal-text">Maaf, jenis ini tidak dapat dihapus karena digunakan oleh koleksi..</p>
 								<?php } ?>
+								
 								<label class="paper-btn btn-small btn-primary" for="modal-<?php echo $dapat->id; ?>">Batal</label>
 							</div>
 						</div>
